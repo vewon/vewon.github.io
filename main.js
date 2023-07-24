f176a5b4e8699215b3f5298651a9d3db5365e744
vewon <vewon@users.noreply.github.com>
2023-07-24 11:26:28 +0800

    add more code

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    const style = window.getComputedStyle(sidebar);
-    const transform = style.getPropertyValue('transform');
-
-    if(transform === "translateX(-100%)") {
-        sidebar.style.transform = "translateX(0)";
-    } else {
-        sidebar.style.transform = "translateX(-100%)";
-    }
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     function toggleSidebar() {
         const sidebar = document.getElementById('sidebar');
+        const style = window.getComputedStyle(sidebar);
+        const transform = style.getPropertyValue('transform');
 
-        if(sidebar.style.transform === "translateX(-100%)") {
+        if(transform === "matrix(1, 0, 0, 1, -100, 0)") {  // translateX(-100%) is represented as matrix(1, 0, 0, 1, -100, 0)
             sidebar.style.transform = "translateX(0)";
         } else {
             sidebar.style.transform = "translateX(-100%)";
